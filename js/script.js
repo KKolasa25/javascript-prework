8563ce47d554ce4ca7a6d23092eac59a705da919
Kamil Kolasa <kamilkolasa19@gmail.com>
2020-03-24 16:48:48 +0100

    changes on js

--- js/script.js
+++ js/script.js
@@ -9,28 +9,18 @@ buttonScissors = document.getElementById('button-scissors');
 function buttonClicked(argButtonName) {
     clearMessages();
     console.log(argButtonName + ' został kliknięty');
-    function printMessage(msg) {
-        var div = document.createElement('div');
-        div.innerHTML = msg;
-        document.getElementById('messages').appendChild(div);
-    }
-
-    function clearMessages() {
-        document.getElementById('messages').innerHTML = '';
-    }
     
-    // Czy to jest w ogóle potrzebne? //
-    //var computerMove, randomNumber;
-    //randomNumber = Math.floor(Math.random() * 3 + 1);
-    //console.log('wylosowana liczba to: ' + randomNumber);
-    //if (randomNumber == '1') {
-    //    computerMove = 'kamień';
-    //} else if (randomNumber == '2') {
-    //    computerMove = 'papier';
-    //} else if (randomNumber == '3') {
-    //    computerMove = 'nożyce';
-    //}
-    //printMessage('Mój ruch: ' + computerMove);
+    var computerMove, randomNumber;
+    randomNumber = Math.floor(Math.random() * 3 + 1);
+    console.log('wylosowana liczba to: ' + randomNumber);
+    if (randomNumber == '1') {
+        computerMove = 'kamień';
+    } else if (randomNumber == '2') {
+        computerMove = 'papier';
+    } else if (randomNumber == '3') {
+        computerMove = 'nożyce';
+    }
+    printMessage('Mój ruch: ' + computerMove);
     
     var playerMove, argButtonName;
     if (playerInput == argButtonName) {
